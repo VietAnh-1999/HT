553c43a6b40420e5beb827af584b96bedd86f1b2
VietAnh-1999 <nguyenvietanhtdh4@gmail.com>
2025-05-09 10:37:24 +0700

    0905

--- BTT/btclass16.py
+++ BTT/btclass16.py
@@ -1,7 +1,6 @@
 class battery:
     def __init__(self):
         self.energy = 10
-#///////////////////////////
 
     def setEnergy(self):
         while True:
@@ -11,20 +10,57 @@ class battery:
                 print("Sac pin khong dung, vui long sac lai!")
             else:
                 break
-
-#////////////////////////////
-
+        return self.energy
+    
     def getEnergy(self):
         print("Phan tram pin hien tai: {0}".format(self.energy))
 
-#////////////////////////////
-
     def decreaseEnergy(self):
-        self.energy -= 2
+        if  self.energy >= 2:
+            self.energy -= 2
     
 #*****************************************************************
-battery_1 = battery()
-battery_1.setEnergy()
-battery_1.getEnergy()
-battery_1.decreaseEnergy()
-battery_1.getEnergy()
+
+class FlashLamp:
+    def __init__(self):
+        self.status = True
+        self.battery = None
+    def setbattery(self,b):
+        self.battery = b
+    
+    def turnOn(self):
+        if self.battery > 0:
+            self.status = True
+            print("Den bat")
+        else:
+            print("Het Pin")
+
+    def turnOff(self):
+        self.status = False
+        print("Den tat *************")
+
+def main():
+
+    pin = battery()
+
+    nhayden = FlashLamp()
+    pin.setEnergy()
+    nhayden.setbattery(pin.energy)
+    print("Da lap pin")
+    print("Phan tram pin hien tai la: {0}".format(nhayden.battery))
+
+    for i in range(20):
+        if nhayden.status == True:
+            nhayden.turnOff()
+        else:
+            nhayden.turnOn()
+            pin.decreaseEnergy()
+            nhayden.setbattery(pin.energy)
+
+    print("Phan tram pin hien tai la: {0}".format(nhayden.battery))
+
+    
+    
+
+if __name__ == "__main__":
+    main()
