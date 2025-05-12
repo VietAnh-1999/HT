f4bd3fad9addbca060f86252ea6db2d0fea534aa
VietAnh-1999 <nguyenvietanhtdh4@gmail.com>
2025-05-12 14:34:07 +0700

    1205

--- BTT/BT19.py
+++ BTT/BT19.py
@@ -1,4 +1,6 @@
+import json
 #tao class BankAcount
+data = []
 class BankAccount:
     def __init__(self,accountNumber,accountName):
         self.accountNumber = accountNumber
@@ -26,7 +28,13 @@ class BankAccount:
             print("Da rut {}đ tu tai khoan".format(money))
         else:
             print("Tai khoan khong du tien")
-        
+    def to_dict(self):
+        return{
+            'accountName': self.accountName,
+            'accountNumber': self.accountNumber,
+            'balance': self.balance
+        }
+
 class Bank:
     def __init__(self):
         self.List_Bankaccount = []
@@ -35,13 +43,60 @@ class Bank:
 
         self.List_Bankaccount.append(account)
 
+
+def write_json():
+    with open("dulieuNH.json","w",encoding= "utf-8") as f:
+        json.dump(data,f,indent=4)
+        print("Da xuat du lieu ra file json")
+
+# def read_json():
+#     try:
+#         with open("dulieuNH.json","r",encoding= "utf-8") as f:
+#             data = json.load(f)
+#             return data
+#         print("Da doc du lieu ra file json")
+#     except FileNotFoundError:
+#         print("Khong tim duoc file hoac file khong ton tai")
+#     except json.JSONDecodeError:
+#         print("file loi")      
+
 def main():
     bank = Bank()
-    acc1 = BankAccount(123456,"VA1999")
-    acc2 = BankAccount(456789556,"VA20001")
+    N = int(input("Nhap vao so tai khoan can tao: "))
+    i = 0
+   # read_json()
+    while i < N:
+        try:
+            acc_num = input("Nhap vao acc_number(6 so): ")
+            if not acc_num.isdigit():
+                print("Vui long chi nhap so")
+                continue
+            if len(acc_num) != 6:
+                print("khac sau ky tu")
+                continue
+
+        except KeyboardInterrupt:
+            break
+        except:
+            print("Dinh dang chua dung. Vui long nhap lai")
+
+        try:
+            acc_name = input("Nhap vao acc_name(it nhat 8 ky tu): ")
+            
+            if len(acc_name) < 8:
+                print("chua du 8 ky tu")
+                continue
+        except KeyboardInterrupt:
+            break
+        except:
+            print("Dinh dang chua dung. Vui long nhap lai")
+
+        acc = BankAccount(acc_num,acc_name)
+        bank.addAccount(acc)
+        data.append(acc.to_dict())
+        write_json()
+        i += 1
     
-    bank.addAccount(acc1)
-    bank.addAccount(acc2)
     for acc in bank.List_Bankaccount:
         print(acc)
     
